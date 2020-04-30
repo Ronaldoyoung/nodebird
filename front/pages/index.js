24cb7881b6b0e35baac8e04533fad7570df162e5
jimmy <jimmy@undefined.gg>
2020-04-30 16:39:45 +0900

    _app.js 로 레이아웃 분리

--- front/pages/index.js
+++ front/pages/index.js
@@ -5,13 +5,7 @@ import AppLayout from '../components/AppLayout';
 const Home = () => {
   return (
     <>
-      <Head>
-        <title>NodeBird</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.css" />
-      </Head>
-      <AppLayout>
-        <div>Hello, Next!</div>
-      </AppLayout>
+      <div>Hello, Next!</div>
     </>
   );
 };
