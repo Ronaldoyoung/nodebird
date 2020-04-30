bf7fb5d6a0d65add5fecea00e489703f05459b9a
jimmy <jimmy@undefined.gg>
2020-04-30 18:18:25 +0900

    layout card 추가 및 액션 추가

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import Link from 'next/link';
-import { Menu, Input, Button } from 'antd';
+import PropTypes from 'prop-types';
+import { Menu, Input, Button, Row, Col, Card, Avatar } from 'antd';
+
+const dummy = {
+  nickname:'지미',
+  Post : [],
+  Following: [],
+  Followers: [],
+}
 
 const AppLayout = ({ children }) => {
   return (
@@ -13,9 +21,33 @@ const AppLayout = ({ children }) => {
         </Menu.Item>
       </Menu>
       <Link href="/signup"><a><Button>회원가입</Button></a></Link>
+      <Row>
+        <Col xs={24} md={6}>
+          <Card
+            actions={[
+              <div key='twit'>짹짹<br/>{dummy.Post.length}</div>,
+              <div key='following'>팔로잉<br/>{dummy.Following.length}</div>,
+              <div key='follower'>팔로워<br/>{dummy.Followers.length}</div>,
+            ]}
+          >
+            <Card.Meta
+              avatar={<Avatar>{dummy.nickname[0]}</Avatar>}
+              title={dummy.nickname}
+            />
+          </Card>
+        </Col>
+        <Col xs={24} md={12}>두번째
+        </Col>
+        <Col xs={24} md={6}>세번째
+        </Col>
+      </Row>
       {children}
     </div>
   );
 };
+
+AppLayout.PropTypes = {
+  children : PropTypes.node,
+}
 
 export default AppLayout;
