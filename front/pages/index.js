c56f773da9ea861f2f77af73b46cdee262527ba0
jimmy <ronaldoyoung@gmail.com>
2020-05-01 17:46:45 +0900

    react-redux 훅 사용

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React , {useEffect} from 'react';
 import PostCard from '../components/PostCard';
 import PostForm from '../components/PostForm';
+import { useDispatch , useSelector } from 'react-redux';
+import {loginAction, logoutACtion,  LOG_IN } from '../reducers/user';
 
 const dummy = {
   isLoggedIn : true,
@@ -17,8 +19,19 @@ const dummy = {
 
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const {isLoggedIn, user } = useSelector( state => state.user);
+
+  useEffect( () => {
+    dispatch(loginAction);
+    dispatch(logoutACtion);
+    dispatch(loginAction);
+
+  } , []);
+
   return (
     <>
+      {user ? <div>로그인 했습니다 : {user.nickname}</div> : <div>로그아웃 했습니다.</div>}
       {dummy.isLoggedIn && <PostForm/>}
       {dummy.mainPosts.map((c) => {
         return (
