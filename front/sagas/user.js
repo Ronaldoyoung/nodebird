7a82d4394e8b256a2c6bc5c5f20be539d82d18df
jimmy <ronaldoyoung@gmail.com>
2020-05-02 13:33:40 +0900

    리덕스 state 와 action 구조 잡기

--- front/sagas/user.js
+++ front/sagas/user.js
@@ -1,5 +1,5 @@
 import { all, fork, takeLatest, call, put } from 'redux-saga/effects';
-import { LOG_IN, LOG_IN_SUCCESS, LOG_IN_FAILURE } from '../reducers/user';
+import { LOG_IN, LOG_IN_SUCCESS, LOG_IN_FAILURE, SIGN_UP_REQUEST } from '../reducers/user';
 
 
 function loginAPI() {
@@ -21,11 +21,35 @@ function* login() {
 }
 
 function* watchLogin() {
-  yield takeLatest(LOG_IN , login);
+  yield takeLatest(LOG_IN_REQUEST , login);
+}
+
+function signUpAPI() {
+  //서버에 요청을 보내는 부분
+  return axios.post('/login');
+}
+
+function* signUp() {
+  try{
+    yield call(signUpAPI);
+    yield put({
+      type:SIGN_UP_SUCCESS
+    })
+  }catch(e){
+    console.error(e);
+    yield put( {
+      type:SIGN_UP_FAILURE
+    })
+  }
+}
+
+function* watchSignUp() {
+  yield takeEvery(SIGN_UP_REQUEST, signUp);
 }
 
 export default function* userSaga() {
   yield all([
     fork(watchLogin),
+    fork(watchSignUp),
   ]);
 }
