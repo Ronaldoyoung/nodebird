c7e317adc004b12c5c3e98096cb8a3dd966d3326
jimmy <ronaldoyoung@gmail.com>
2020-05-03 00:33:00 +0900

    댓글 컴포넌트 만들기

--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import {Card , Icon, Button, Avatar } from 'antd';
+import React , { useState, useCallback } from 'react';
+import {Card , Icon, Button, Avatar, Form, List, Comment, Input} from 'antd';
 import PropTypes from 'prop-types';
+import { useSelector , useDispatch } from 'react-redux';
+import { ADD_COMMENT_REQUEST } from '../reducers/post';
 
 
 const PostCard = ({post}) => {
+  const [commentFormOpened, setCommentFormOpened] = useState(false);
+  const [commentText, setCommentText] = useState('');
+  const { me } = useSelector(state => state.user);
+  const dispatch = useDispatch();
+
+  const onToggleComment = useCallback(() => {
+    setCommentFormOpened(prev => !prev);
+
+  }, []);
+
+  const onSubmitComment = useCallback((e) => {
+    e.preventDefault();
+    if(!me){
+      return alert('???? ?????.');
+    }
+    dispatch({
+      type : ADD_COMMENT_REQUEST,
+    })
+  }, []);
+
+  const onChangeCommnetText = useCallback((e) => {
+    setCommentText(e.target.value);
+  }, []);
+
   return (
+    <div>
     <Card
       key={+post.createAt}
       cover={post.img && <img alt="example" src={post.img} />}
       actions={[
         <Icon type="retweet" key="retwwet" />,
         <Icon type="heart" key="heart" />,
-        <Icon type="message" key="message" />,
+        <Icon type="message" key="message" onClick={onToggleComment}/>,
         <Icon type="ellipsis" key="ellipsis" />,
       ]}
       extra={<Button>팔로우</Button>}
@@ -23,6 +50,32 @@ const PostCard = ({post}) => {
       />
 
     </Card>
+      {commentFormOpened && (
+        <>
+          <Form onSubmit={onSubmitComment}>
+            <Form.Item>
+              <Input.TextArea row={4} value={commentText} onChange={onChangeCommnetText} />
+            </Form.Item>
+            <Button type='primary' htmlType="submit">??</Button>
+          </Form>
+          <List
+            header={`${post.Comments ? post.Comments.length : 0} ??`}
+            itemLayout ='horizontal'
+            dataSource={post.Comment || []}
+            renderItem={item => (
+              <li>
+                <Comment
+                  author={item.User.nickname}
+                  avatar={<Avatar>{item.User.nickname[0]}</Avatar>}
+                  content={item.content}
+                  datetime={item.createAt}
+                />
+              </li>
+            )}
+          />
+        </>
+      )}
+    </div>
   )
 }
 
